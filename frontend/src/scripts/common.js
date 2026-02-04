c4211ad6e69a73c90c8d0c9d082fce9c80f2bab8
nielsen00 <73450322+nielsen00@users.noreply.github.com>
2026-02-04 14:26:00 -0600

    Move openAlert to common

--- frontend/src/scripts/common.js
+++ frontend/src/scripts/common.js
@@ -92,3 +92,34 @@ export function activatePaletteButton(buttonID) {
     }
 	document.getElementById(buttonID).classList.add("selectedButton");
 }
+
+/**
+ * Open the alert tab, displaying the given message and creating buttons 
+ * displaying the given names and with the given functions as their callbacks
+ * @param {string} message - the message to be displayed with the alert
+ * @param {string[]} buttonNames - the names of the buttons, to be displayed
+ * @param {function[]} buttonFunctions - the functions to be used as callbacks
+ * 		for each button, 
+ */
+export function openAlert(message, buttonNames = [], buttonFunctions = []) {
+	const alertTab = document.getElementById("alertTab");
+	alertTab.innerHTML = "";
+
+	const messageParagraph = document.createElement("p");
+	messageParagraph.style.textAlign = "center";
+	messageParagraph.innerHTML = message;
+
+	const buttonRow = document.createElement("div");
+	for (let i = 0; i < buttonNames.length; i++) {
+		const name = buttonNames[i];
+		const button = document.createElement("button");
+		button.innerHTML = name;
+		button.onpointerup = buttonFunctions[i];
+		buttonRow.appendChild(button);
+	}
+
+	alertTab.appendChild(messageParagraph);
+	alertTab.appendChild(buttonRow);
+
+	COM.openSidebarTab("alertTab");
+}	
